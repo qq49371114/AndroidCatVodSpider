d218a7147181a7bac41008fdbed461c31b7a34d0
lushunming <1357197829@qq.com>
2024-08-01 18:54:33 +0800

    libvio

---
+++ app/src/test/java/LibvioTest.java
@@ -0,0 +1,96 @@
+import android.app.Application;
+
+import com.github.catvod.spider.DaGongRen;
+import com.github.catvod.spider.Init;
+import com.github.catvod.spider.Libvio;
+import com.github.catvod.utils.Json;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RunWith(RobolectricTestRunner.class)
+public class LibvioTest {
+    // @Mock
+    private Application mockContext;
+
+    private Libvio spider;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        mockContext = RuntimeEnvironment.application;
+        Init.init(mockContext);
+        spider = new Libvio();
+        spider.init(mockContext, "https://libvio.app");
+    }
+
+    @org.junit.Test
+    public void homeContent() throws Exception {
+        String content = spider.homeContent(true);
+        JsonObject map = Json.safeObject(content);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        System.out.println("homeContent--" + gson.toJson(map));
+
+        //Assert.assertFalse(map.getAsJsonArray("list").isEmpty());
+    }
+
+    @org.junit.Test
+    public void homeVideoContent() throws Exception {
+        String content = spider.homeVideoContent();
+        JsonObject map = Json.safeObject(content);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        System.out.println("homeVideoContent--" + gson.toJson(map));
+
+        //Assert.assertFalse(map.getAsJsonArray("list").isEmpty());
+    }
+
+    @org.junit.Test
+    public void categoryContent() throws Exception {
+        String content = spider.categoryContent("/type/1", "2", true, null);
+        JsonObject map = Json.safeObject(content);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println("categoryContent--" + gson.toJson(map));
+        Assert.assertFalse(map.getAsJsonArray("list").isEmpty());
+    }
+
+    @org.junit.Test
+    public void detailContent() throws Exception {
+
+        String content = spider.detailContent(Arrays.asList("714891769.html"));
+        JsonObject map = Json.safeObject(content);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println("detailContent--" + gson.toJson(map));
+        Assert.assertFalse(map.getAsJsonArray("list").isEmpty());
+    }
+
+    @org.junit.Test
+    public void playerContent() throws Exception {
+        String froms = "BD播放$$$视频下载 (夸克)";
+        String urls = "第01集$714891769-1-1.html#第02集$714891769-1-2.html#第03集$714891769-1-3.html#第04集$714891769-1-4.html$$$合集$714891769-2-1.html";
+        for (int i = 0; i < urls.split("\\$\\$\\$").length; i++) {
+            String content = spider.playerContent(froms.split("\\$\\$\\$")[i], urls.split("\\$\\$\\$")[i].split("#")[0].split("\\$")[1], new ArrayList<>());
+            JsonObject map = Json.safeObject(content);
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            System.out.println("playerContent--" + gson.toJson(map));
+            Assert.assertFalse(map.getAsJsonPrimitive("url").getAsString().isEmpty());
+        }
+    }
+
+    @org.junit.Test
+    public void searchContent() throws Exception {
+        String content = spider.searchContent("红海", false);
+        JsonObject map = Json.safeObject(content);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println("searchContent--" + gson.toJson(map));
+        Assert.assertFalse(map.getAsJsonArray("list").isEmpty());
+    }
+}
