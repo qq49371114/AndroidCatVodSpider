f032305bc7fe50ec192fe206afb81bef961bd943
“lushunming” <lusm@oceansoft.com.cn>
2024-10-05 20:26:08 +0800

    网盘搜索

--- app/src/main/java/com/github/catvod/spider/PanSearch.java
+++ app/src/main/java/com/github/catvod/spider/PanSearch.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import org.jsoup.Jsoup;
 
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +19,7 @@ import java.util.Map;
 /**
  * @author zhixc
  */
-public class PanSearch extends Ali {
+public class PanSearch extends Cloud {
 
     private final String URL = "https://www.pansearch.me/";
 
@@ -40,21 +41,26 @@ public class PanSearch extends Ali {
         String html = OkHttp.string(URL, getHeader());
         String data = Jsoup.parse(html).select("script[id=__NEXT_DATA__]").get(0).data();
         String buildId = new JSONObject(data).getString("buildId");
-        String url = URL + "_next/data/" + buildId + "/search.json?keyword=" + URLEncoder.encode(key) + "&pan=aliyundrive";
-        String result = OkHttp.string(url, getSearchHeader());
-        JSONArray array = new JSONObject(result).getJSONObject("pageProps").getJSONObject("data").getJSONArray("data");
+        List<String> types = List.of("quark", "aliyundrive");
         List<Vod> list = new ArrayList<>();
-        for (int i = 0; i < array.length(); i++) {
-            JSONObject item = array.getJSONObject(i);
-            String content = item.optString("content");
-            String[] split = content.split("\\n");
-            if (split.length == 0) continue;
-            String vodId = Jsoup.parse(content).select("a").attr("href");
-            String name = split[0].replaceAll("</?[^>]+>", "");
-            String remark = item.optString("time");
-            String pic = item.optString("image");
-            list.add(new Vod(vodId, name, pic, remark));
+
+        for (String type : types) {
+            String url = URL + "_next/data/" + buildId + "/search.json?keyword=" + URLEncoder.encode(key, Charset.defaultCharset().name()) + "&pan=" + type;
+            String result = OkHttp.string(url, getSearchHeader());
+            JSONArray array = new JSONObject(result).getJSONObject("pageProps").getJSONObject("data").getJSONArray("data");
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject item = array.getJSONObject(i);
+                String content = item.optString("content");
+                String[] split = content.split("\\n");
+                if (split.length == 0) continue;
+                String vodId = Jsoup.parse(content).select("a").attr("href");
+                String name = split[0].replaceAll("</?[^>]+>", "");
+                String remark = item.optString("time");
+                String pic = item.optString("image");
+                list.add(new Vod(vodId, name, pic, remark));
+            }
         }
+
         return Result.string(list);
     }
 }
