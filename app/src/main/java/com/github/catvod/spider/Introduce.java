7085d351e66d01a57674bc433c739593e813ad80
lushunming <1357197829@qq.com>
2024-11-20 11:30:33 +0800

    更新项目介绍

--- app/src/main/java/com/github/catvod/spider/Introduce.java
+++ app/src/main/java/com/github/catvod/spider/Introduce.java
@@ -20,12 +20,12 @@ public class Introduce extends Spider {
     @Override
     public String homeContent(boolean filter) throws Exception {
         List<Vod> list = new ArrayList<>();
-        String pic = "https://s21.ax1x.com/2024/11/20/pAWG1n1.jpg";
+        String pic = "https://s21.ax1x.com/2024/11/20/pAWGU9e.png";
         String name = "关注公众号";
-        list.add(new Vod("https://s21.ax1x.com/2024/11/20/pAWG1n1.jpg", name, pic));
-        String pic2 = "https://s21.ax1x.com/2024/11/20/pAWG1n1.jpg";
+        list.add(new Vod("https://s21.ax1x.com/2024/11/20/pAWGU9e.png", name, pic));
+        String pic2 = "https://s21.ax1x.com/2024/11/20/pAWGU9e.png";
         String name2 = "本接口不收费，请不要付费，谢谢！";
-        list.add(new Vod("https://s21.ax1x.com/2024/11/20/pAWG1n1.jpg", name2, pic2));
+        list.add(new Vod("https://s21.ax1x.com/2024/11/20/pAWGU9e.png", name2, pic2));
         return Result.string(list);
     }
 
