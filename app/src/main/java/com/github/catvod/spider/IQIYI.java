587bb3f8fea15a9d09e22676e4659e173ef77b46
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2024-11-22 21:40:33 +0800

    更新 IQIYI.java

--- app/src/main/java/com/github/catvod/spider/IQIYI.java
+++ app/src/main/java/com/github/catvod/spider/IQIYI.java
@@ -1,30 +1,19 @@
-import android.content.Context;
+package com.github.catvod.spider;
 
-import com.github.catvod.bean.Class;
-import com.github.catvod.bean.Result;
-import com.github.catvod.bean.Vod;
+import android.content.Context;
+import android.net.Uri;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
-import com.github.catvod.net.OkHttp;
-import com.github.catvod.utils.Json;
-import com.github.catvod.utils.Util;
+import com.github.catvod.utils.okhttp.OkHttpUtil;
 
-import com.google.gson.JsonElement;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.net.URLEncoder;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Set;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class IQIYI extends Spider {
     protected JSONObject a;
@@ -293,4 +282,3 @@ public class IQIYI extends Spider {
         return jSONObject2.toString();
     }
 }
-
