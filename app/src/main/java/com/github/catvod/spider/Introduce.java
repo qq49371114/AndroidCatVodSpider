7dbb51a5514a7a4665cd14551b26ced808fbab66
qq49371114 <143635592+qq49371114@users.noreply.github.com>
2025-06-05 00:32:10 +0800

    Update Introduce.java

--- app/src/main/java/com/github/catvod/spider/Introduce.java
+++ app/src/main/java/com/github/catvod/spider/Introduce.java
@@ -28,15 +28,15 @@ public class Introduce extends Spider {
         classes.add(new Class("1", "UC"));
         classes.add(new Class("2", "天意"));
         List<Vod> list = new ArrayList<>();
-        String pic = "https://androidcatvodspider.netlify.app/wechat.png";
-        String name = "关注公众号";
-        list.add(new Vod("https://androidcatvodspider.netlify.app/wechat.png", name, pic));
-        String pic2 = "https://androidcatvodspider.netlify.app/wechat.png";
-        String name2 = "本接口不收费，请不要付费，谢谢！";
-        list.add(new Vod("https://androidcatvodspider.netlify.app/wechat.png", name2, pic2));
-        String pic3 = "https://androidcatvodspider.netlify.app/wechat.png";
+        String pic = "http://47.109.61.116:86/logo/yylx.gif";
+        String name = "遥遥领先";
+        list.add(new Vod("http://47.109.61.116:86/logo/yylx.gif", name, pic));
+        String pic2 = "http://47.109.61.116:86/logo/yylx.gif";
+        String name2 = "本接口专用，谢谢！";
+        list.add(new Vod("http://47.109.61.116:86/logo/yylx.gif", name2, pic2));
+        String pic3 = "http://47.109.61.116:86/logo/yylx.gif";
         String name3 = "2025-04-14 11:00";
-        list.add(new Vod("https://androidcatvodspider.netlify.app/wechat.png", name3, pic3));
+        list.add(new Vod("http://47.109.61.116:86/logo/yylx.gif", name3, pic3));
         return Result.string(classes, list);
     }
 
@@ -46,16 +46,16 @@ public class Introduce extends Spider {
         List<Vod> vodList = new ArrayList<>();
         //UC
         if (tid.equals("1")) {
-            String pic = "https://androidcatvodspider.netlify.app/wechat.png";
+            String pic = "http://47.109.61.116:86/logo/yylx.gif";
             String name = "点击设置Token";
             vodList.add(new Vod("UCToken", name, pic));
         }
         //天翼
         if (tid.equals("2")) {
-            String pic = "https://androidcatvodspider.netlify.app/wechat.png";
+            String pic = "http://47.109.61.116:86/logo/yylx.gif";
             String name = "点击设置cookie";
             vodList.add(new Vod("天翼cookie", name, pic));
-            String pic1 = "https://androidcatvodspider.netlify.app/wechat.png";
+            String pic1 = "http://47.109.61.116:86/logo/yylx.gif";
             String name1 = "清除cookie";
             vodList.add(new Vod("clean天翼cookie", name1, pic1));
         }
@@ -82,12 +82,12 @@ public class Introduce extends Spider {
         }
         Vod item = new Vod();
         item.setVodId(vodId);
-        item.setVodName("公众号");
-        item.setVodPic("https://androidcatvodspider.netlify.app/wechat.png");
+        item.setVodName("遥遥领先");
+        item.setVodPic("http://47.109.61.116:86/logo/yylx.gif");
         item.setVodRemarks("");
-        item.setVodPlayFrom("公众号");
+        item.setVodPlayFrom("遥遥领先");
         item.setVodPlayUrl("https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8");
-        item.setVodDirector("公众号");
+        item.setVodDirector("遥遥领先");
         return Result.string(item);
     }
 
